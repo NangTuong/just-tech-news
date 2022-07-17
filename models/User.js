337b0cc460f6e176032f891d7510e4831258ad2e
NangTuong <nguyen.brad@outlook.com>
2022-07-17 11:37:58 -0400

    installed bcrypt to hash newly created/updated passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -8,15 +9,11 @@ class User extends Model {}
 User.init(
   {
     id: {
-      // use the special Sequelize DataTypes object provide what type of data it is 
       type: DataTypes.INTEGER,
       allowNull: false,
-      // instruct that this is the Primary Key 
       primaryKey: true,
-      // turn on auto increment
       autoIncrement: true
     },
-    // define a username column
     username: {
       type: DataTypes.STRING,
       allowNull: false
@@ -24,24 +21,32 @@ User.init(
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      // there cannot be any duplicate email values in this table
       unique: true,
-      // if allowNull is set to false, we can run our data through validators before creating the table data
       validate: {
         isEmail: true
       }
     },
-    // define a password column 
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        // this means the password must be at least four characters long 
         len: [4]
       }
     }
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
